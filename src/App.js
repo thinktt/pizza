6b431547e47e2b24322ec3ab7a4e04305a5bcd6a
Tobias Toland <github@thinktt.e4ward.com>
2023-05-16 22:38:07 -0500

    intitial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const pizzas = ['Peperoni', 'Mushroom', 'Pinapple', 'Cheese']
+  const crust = ['Thin Crust', 'Deep Dish']
+  const size = ['Small', 'Medium', 'Large', 'Extra Large']
+  const table = ['1', '2', '3', '4', '5', '6', '7', '8']
+  return (
+    <main className="main">
+      <h1 className="title">
+          Turtle Pizza
+      </h1>
+      <h4>appears at your table with ninja stealth</h4>
+      <img 
+        className="turtles" 
+        src="/tmnt.png" alt="A group of teenage turtles approving of pizza" />
+      <div className="pizzas option-group">
+        { pizzas.map(pizza => 
+          <h2 key={pizza} className="selected">
+            {pizza}
+          </h2> ) 
+        }
+      </div>
+      <div className="crust option-group">
+        { crust.map(crust => <h2 key={crust}> {crust} </h2> ) }
+      </div>
+      <div key={size} className="size option-group">
+        { size.map(size => <h2 key={size}> {size} </h2> ) }
+      </div>
+      <div className="table option-group">
+        { table.map(table => <h2 key={table} >{table} </h2> ) }
+      </div>
+    </main>
+  )
+}
+
+export default App
+
+
