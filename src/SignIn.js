44a88315f225366ceb124a9339a35e6244bdb6e4
Tobias Toland <github@thinktt.e4ward.com>
2023-05-17 09:18:26 -0500

    progress on signin, need form to submit

--- src/SignIn.js
+++ src/SignIn.js
@@ -5,13 +5,23 @@ export default function SignIn() {
   const [username, setUsername] = useState()
   const [password, setPassword] = useState()
   const [error, setError] = useState()
+  const [isSending, setIsSending] = useState(false)
   
   const sendAuth = async (username, password) => {
+    setError(null)
+    setIsSending(true)
+    // await new Promise(r => setTimeout(r, 2000))
+    
     let err
     await api.doAuth({ username, password }).catch((e) => err = e)
     if (err) {
       setError(err)
+      setIsSending(false)
+      return 
     }
+    
+    setIsSending(false)
+    setError({message: 'bad username or password'})
   }
 
   return (
@@ -24,18 +34,15 @@ export default function SignIn() {
         className="turtles" 
         src="/tmnt.png" alt="A group of teenage turtles approving of pizza" 
       />
-      <h1>{error}</h1>
-      { 
-        !error && 
+      { isSending ? <h1>spinner...</h1> : 
         <SignInForm 
           username={username} 
           password={password} 
           sendAuth={sendAuth}
         >
         </SignInForm> 
-      }      
-
-
+      } 
+      { error && <h1>{error.message}</h1> }     
     </main>
   )
 }
@@ -43,11 +50,11 @@ export default function SignIn() {
 
 function SignInForm({ username, password, sendAuth}) {
   return (
-    <form>
+    <div>
       <input type="text" placeholder="Username" value={username}></input>
       <input type="password" placeholder="Password" value={password}></input>
       <button onClick={() => sendAuth(username, password)}>Sign In</button>
-    </form>
+    </div>
   )
 }
 
